f05ec5f22122d43de7f0b557019806b805f3bd66
G984302019 <198430@st.takushoku-u.ac.jp>
2022-10-17 19:34:42 +0900

    [[commit!!]]

--- GSA.java
+++ GSA.java
@@ -788,7 +788,7 @@ public class GSA implements LocalTransformer {
 //    	  }
 //      }
       
-      localCodeMotion();
+//      localCodeMotion();
 //      globalCodeMotion();
       displayBasicBlk();
       testGCM();
